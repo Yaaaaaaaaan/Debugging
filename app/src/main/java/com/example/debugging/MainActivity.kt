065ddf634c37f7e0a0e2dc21af4b1d2067a733f2
Yan Fonseca <yangoular@gmail.com>
2022-05-06 06:57:30 -0300

    Aprendendo a Debugar código Kotlin com Android Studio

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -10,9 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         logging()
+        division()
+        }
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
     }
     fun logging(){
-
             Log.e(TAG, "ERROR: a serious error like an app crash")
             Log.w(TAG, "WARN: warns about the potential for serious errors")
             Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
