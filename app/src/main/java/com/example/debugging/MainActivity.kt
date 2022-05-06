1fd84b1f19ef9c9c26be0ae35fb18be5b35b5f1a
Yan Fonseca <yangoular@gmail.com>
2022-05-06 09:16:38 -0300

    Aprendendo a Debugar código Kotlin com Android Studio

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -9,9 +9,9 @@ private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         val helloTextView: TextView=findViewById(R.id.hello_world)
         helloTextView.text="Hello, debugging!"
-        setContentView(R.layout.activity_main)
         logging()
         division()
         }
